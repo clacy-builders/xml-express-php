2017d78ebe354ffa26282ba71f6f70e70a07ef1e
M. Hoffmann <git@applications-cologne.com>
2015-11-15 10:38:11 +0100

    XLink, XLinkConstants in separate files (according to psr-4)

---
+++ src/methods/XLinkConstants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ML_Express;
+
+interface XLinkConstants
+{
+	const XLINK_TYPE_SIMPLE = 'simple';
+
+	const XLINK_SHOW_EMBED = 'embed';
+	const XLINK_SHOW_NEW = 'new';
+	const XLINK_SHOW_REPLACE = 'replace';
+	const XLINK_SHOW_OTHER = 'other';
+	const XLINK_SHOW_NONE = 'none';
+
+	const XLINK_ACTUATE_ONLOAD = 'onLoad';
+	const XLINK_ACTUATE_ONREQUEST = 'onRequest';
+	const XLINK_ACTUATE_OTHER = 'other';
+	const XLINK_ACTUATE_NONE = 'none';
+}
