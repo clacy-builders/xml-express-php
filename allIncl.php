c64a9d55053c83544d227b4c98bec4632cc73339
M. Hoffmann <git@applications-cologne.com>
2016-02-16 19:14:24 +0100

    allIncl.php updated

--- allIncl.php
+++ allIncl.php
@@ -10,6 +10,8 @@ require_once __DIR__ . '/src/Adhoc.php';
 require_once __DIR__ . '/src/shared/addQuery.php';
 require_once __DIR__ . '/src/shared/ClassAttribute.php';
 require_once __DIR__ . '/src/shared/DimensionAttributes.php';
+require_once __DIR__ . '/src/shared/MediaAttribute.php';
+require_once __DIR__ . '/src/shared/MediaAttributeConstants.php';
 require_once __DIR__ . '/src/shared/StyleAttribute.php';
 require_once __DIR__ . '/src/shared/XLink.php';
 require_once __DIR__ . '/src/shared/XLinkConstants.php';
