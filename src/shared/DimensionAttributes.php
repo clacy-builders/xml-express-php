a7e38b3bd1f71fec883dd4e91059ab778e5bdb8f
M. Hoffmann <git@applications-cologne.com>
2016-03-01 23:08:13 +0100

    DocBlocks

--- src/shared/DimensionAttributes.php
+++ src/shared/DimensionAttributes.php
@@ -4,16 +4,35 @@ namespace ML_Express\Shared;
 
 trait DimensionAttributes
 {
+	/**
+	 * Sets the <code>width</code> attribute.
+	 * 
+	 * @param  mixed  $width   CSS pixels (Html5), for SVG also a string: '160px', '10em' etc.
+	 * @return Xml
+	 */
 	public function setWidth($width)
 	{
 		return $this->attrib('width', $width);
 	}
 
+	/**
+	 * Sets the <code>height</code> attribute.
+	 * 
+	 * @param  mixed  $height  CSS pixels (Html5), for SVG also a string: '160px', '10em' etc.
+	 * @return Xml
+	 */
 	public function setHeight($height)
 	{
-		return $this->attrib('height', $height);
+		return $this->setattrib('height', $height);
 	}
 
+	/**
+	 * Sets the <code>width</code> and <code>height</code> attributes.
+	 * 
+	 * @param  mixed  $width   CSS pixels (Html5), for SVG also a string: '160px', '10em' etc.
+	 * @param  mixed  $height  CSS pixels (Html5), for SVG also a string: '160px', '10em' etc.
+	 * @return Xml
+	 */
 	public function setDimensions($width, $height)
 	{
 		return $this->setWidth($width)->setHeight($height);
